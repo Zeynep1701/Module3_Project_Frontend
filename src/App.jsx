52f9c76a0b814a1149cc12a7893d0052d5378ac1
Zeynep <zarazeynep5858@googlemail.com>
2023-10-25 09:03:15 +0200

    Components,Pages, Etc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
-import './App.css'
+import { Link, Route, Routes } from 'react-router-dom'
+import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/LoginPage'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
-  return 
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/signup'>Signup</Link>
+          </li>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+          <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+          <li>
+            <Link to='/books/new'>New book</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<h1>Home Page</h1>} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route
+          path='/profile'
+          element={
+            <PrivateRoute>
+              <ProfilePage />
+            </PrivateRoute>
+          }
+          />                
+        <Route path='*' element={<h1>404 Page</h1>} />
+       
+      </Routes>
+    </>
+  )
 }
 
 export default App
