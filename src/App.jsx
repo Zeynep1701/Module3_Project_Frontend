55aa0286170f1e4eaa829ed7a9376c88ae3eb4da
Nadia Escobar <escobarnad@gmail.com>
2023-10-25 12:05:42 +0200

    Sign up working and connected with backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { Link, Route, Routes } from 'react-router-dom'
-import SignupPage from './pages/SignupPage'
-import LoginPage from './pages/LoginPage'
-import PrivateRoute from './components/PrivateRoute'
+import { Link, Route, Routes } from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -9,39 +9,38 @@ function App() {
       <nav>
         <ul>
           <li>
-            <Link to='/'>Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to='/signup'>Signup</Link>
+            <Link to="/signup">Signup</Link>
           </li>
           <li>
-            <Link to='/login'>Login</Link>
+            <Link to="/login">Login</Link>
           </li>
           <li>
-            <Link to='/profile'>Profile</Link>
+            <Link to="/profile">Profile</Link>
           </li>
           <li>
-            <Link to='/books/new'>New book</Link>
+            <Link to="/books/new">New book</Link>
           </li>
         </ul>
       </nav>
       <Routes>
-        <Route path='/' element={<h1>Home Page</h1>} />
-        <Route path='/signup' element={<SignupPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route
-          path='/profile'
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route
+          path="/profile"
           element={
             <PrivateRoute>
               <ProfilePage />
             </PrivateRoute>
           }
-          />                
-        <Route path='*' element={<h1>404 Page</h1>} />
-       
+        /> */}
+        <Route path="*" element={<h1>404 Page</h1>} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
