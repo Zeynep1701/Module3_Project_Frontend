436dc33f1423b0b1032371ff7caaaf22e01a4065
Zeynep <zarazeynep5858@googlemail.com>
2023-10-26 17:46:16 +0200

    ProfilePage but is not finished

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,47 +1,68 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { AuthContext } from '../contexts/AuthContext';
+import { AuthContext } from './contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
 const ProfilePage = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
+    const { token, userId } = useContext(AuthContext);
 
-  const [userName, setUserName] = useState('');
-  const [email, setEmail] = useState('');
-  const [image, setImage] = useState('');
-  const { fetchWithToken, isLoading, isAuthenticated } = useContext(AuthContext);
-
-  useEffect(() => {
+    const [userName, setUserName] = useState('');
+    const [email, setEmail] = useState('');
+    const [image, setImage] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+    
+    const fetchWithToken = async (endpoint, callback, method = 'GET', body) => {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api${endpoint}`, {
+                method,
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body),
+            });
+            
+            if (response.ok) {
+                const parsed = await response.json();
+                callback(parsed);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    
     const fetchData = async () => {
-      const userData = await fetchWithToken('/user/profile');
-      if (userData) {
-        setUserName(userData.userName);
-        setEmail(userData.email);
-        setImage(userData.image);
-      }
+        fetchWithToken(`/user/${userId}`, (data) => {
+            setUserName(data.userName);
+            setEmail(data.email);
+            setImage(data.image);
+            setIsLoading(false);
+        });
     };
 
-    if (isAuthenticated) {
-      fetchData();
+    useEffect(() => {
+        if (userId) { 
+            fetchData();
+        }
+    }, [userId]);
+
+    if (isLoading) {
+        return <p>Loading...</p>;
     }
-  }, [isAuthenticated, fetchWithToken]);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
+    if (!userId) {
+        navigate('/login');
+        return null;
+    }
 
-  if (!isAuthenticated) {
-    navigate("/login");
-    return null;
-  }
-
-  return (
-    <div>
-      <h1>My Profile</h1>
-      <img src={image} alt={userName} style={{ width: '100px', height: '100px' }} />
-      <p>UserName: {userName}</p>
-      <p>Email: {email}</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>My Profile</h1>
+            <img src={image} alt={userName} style={{ width: '100px', height: '100px' }} />
+            <p>UserName: {userName}</p>
+            <p>Email: {email}</p>
+        </div>
+    );
 };
 
 export default ProfilePage;
