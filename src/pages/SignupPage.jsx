d72604b2004879a9dbaeaf18ac2cab401f3c7317
Nadia Escobar <escobarnad@gmail.com>
2023-10-30 11:50:36 +0100

    Cloudinary working

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -5,23 +5,33 @@ const SignupPage = () => {
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [image, setImage] = useState("");
+  // const [image, setImage] = useState("");
 
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const payload = { userName, email, password, image };
+
+    const myImage = event.target.image.files[0];
+    const myFormData = new FormData();
+    myFormData.append("image", myImage);
+    myFormData.append("userName", userName);
+    myFormData.append("email", email);
+    myFormData.append("password", password);
+
+    const payload = myFormData;
+    console.log(myFormData);
 
     try {
       const response = await fetch(
         `${import.meta.env.VITE_API_URL}/auth/signup`,
         {
           method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(payload),
+          // headers: {
+          //   "Content-Type": "multipart/form-data",
+          //   boundary=something
+          // },
+          body: payload,
         }
       );
       if (response.status === 201) {
@@ -41,6 +51,7 @@ const SignupPage = () => {
         <label>
           User Name:
           <input
+            name="userName"
             value={userName}
             onChange={(event) => setUserName(event.target.value)}
             required
@@ -49,6 +60,7 @@ const SignupPage = () => {
         <label>
           Email:
           <input
+            name="email"
             value={email}
             onChange={(event) => setEmail(event.target.value)}
             required
@@ -57,6 +69,7 @@ const SignupPage = () => {
         <label>
           Password
           <input
+            name="password"
             value={password}
             onChange={(event) => setPassword(event.target.value)}
             required
@@ -66,8 +79,10 @@ const SignupPage = () => {
         <label>
           Image:
           <input
-            value={image}
-            onChange={(event) => setImage(event.target.value)}
+            type="file"
+            name="image"
+            // value={image}
+            // onChange={(event) => setImage(event.target.image.files[0])}
           />
         </label>
         <button type="submit">Register</button>
