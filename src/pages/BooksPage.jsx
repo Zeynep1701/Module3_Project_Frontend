d8f7ae8b4bcd4f53b086b2d73100f469acf438c3
Rin-o <rino091370@gmail.com>
2023-10-31 13:20:01 +0100

    filter added to the book list page

--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -2,7 +2,9 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 function BooksPage() {
+  const [originalBooks, setOriginalBooks] = useState([]);
   const [books, setBooks] = useState([]);
+  const [category, setCategory] = useState("all");
 
   const navigate = useNavigate();
 
@@ -18,6 +20,7 @@ function BooksPage() {
       });
       if (response.ok) {
         const allBooks = await response.json();
+        setOriginalBooks(allBooks);
         setBooks(allBooks);
       } else {
         navigate("/login");
@@ -26,12 +29,53 @@ function BooksPage() {
       console.log(error);
     }
   };
+
   useEffect(() => {
     fetchAllBooks();
   }, []);
 
+  //sorting functions to filter books 
+
+  const handleSortByAuthors = () => {
+    const copyBooks = [...books];
+    copyBooks.sort((book1, book2) => book1.authorId.name > book2.authorId.name ? 1 : -1)
+    setBooks(copyBooks);
+  }
+
+  const handleSortByBooks = () => {
+    const copyBooks = [...books];
+    copyBooks.sort((book1, book2) => book1.title > book2.title ? 1 : -1)
+    setBooks(copyBooks);
+  }
+
+  const handleSortByCategories = () => {
+    const copyBooks = [...books];
+    copyBooks.sort((book1, book2) => book1.categories > book2.categories ? 1 : -1)
+    setBooks(copyBooks);
+  }
+
+  /*const handleSortByCategories = (selectedCategory) => {
+    const filteredBooks = selectedCategory === "all" ? originalBooks : originalBooks.filter(book => book.category === selectedCategory);
+    setCategory(selectedCategory);
+    setBooks(filteredBooks);
+  };*/
+
   return (
     <>
+      <button type='button' className="button-74" onClick={() => handleSortByAuthors()}>Sort by Authors</button>
+      <button type='button' className="button-74" onClick={() => handleSortByBooks()}>Sort by Books</button>
+      <button type='button' className="button-74" onClick={() => handleSortByCategories ()}>Sort by Categories</button>
+
+      {/*<p>Sort by Category: {category}</p>
+      <button type='button' className="button-60" onClick={() => handleSortByCategories("All")}>All</button>
+      <button type='button' className="button-60" onClick={() => handleSortByCategories("Romance")}>Romance</button>
+      <button type='button' className="button-60" onClick={() => handleSortByCategories("Fantasy")}>Fantasy</button>
+      <button type='button' className="button-60" onClick={() => handleSortByCategories("Fiction")}>Fiction</button>
+      <button type='button' className="button-60" onClick={() => handleSortByCategories("Horror")}>Horror</button>
+      <button type='button' className="button-60" onClick={() => handleSortByCategories("Mystery")}>Mystery</button>
+      <button type='button' className="button-60" onClick={() => handleSortByCategories("Self-help")}>Self-help</button>
+      <button type='button' className="button-60" onClick={() => handleSortByCategories("Drama")}>Drama</button>
+  */}
       <ul
         style={{
           listStyle: "none",
@@ -54,6 +98,7 @@ function BooksPage() {
               <img src={book.image} style={{ height: "200px" }} />
               <h3>{book.title}</h3>
               <p>Author: {book.authorId.name}</p>
+              <p>Category: {book.categories}</p>
             </Link>
           </li>
         ))}
