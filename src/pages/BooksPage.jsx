311d32d4461a1525058c7e6cbd7d69f72c38fad2
Zeynep <zarazeynep5858@googlemail.com>
2023-11-01 17:17:01 +0100

    Corrected mistake on BooksPage

--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -90,7 +90,7 @@ function BooksPage() {
   return (
     <>
       <h1 className="title">Find what books to read next</h1>
-      <h2>Find yout new books with other readers recommendations</h2>
+      <h2>Find your new books with other readers recommendations</h2>
       <button
         type="button"
         className="button-74 btn"
