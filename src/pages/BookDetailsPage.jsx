15b967e8eefa25e70d3ab56e4dd6a9b80bb91ba2
Nadia Escobar <escobarnad@gmail.com>
2023-11-02 12:25:44 +0100

    working date

--- src/pages/BookDetailsPage.jsx
+++ src/pages/BookDetailsPage.jsx
@@ -4,6 +4,8 @@ import AppCss from "../App.css";
 import ReviewForm from "../components/ReviewForm.jsx";
 import UpdateReviewForm from "../components/UpdateReviewForm";
 import { AuthContext } from "../contexts/AuthContext";
+import { format } from "date-fns";
+import { string } from "prop-types";
 
 function BookDetailsPage() {
   const [book, setBook] = useState(null);
@@ -102,6 +104,24 @@ function BookDetailsPage() {
     }
   };
 
+  const formattedPublishingDate = () => {
+    let publishingDate = book.book.publishingDate;
+    let stringDate = publishingDate.toString();
+    let formattedDate = stringDate.slice(0, 10).split("-");
+    if (formattedDate.length === 3) {
+      return `${formattedDate[2]}-${formattedDate[1]}-${formattedDate[0]}`;
+    }
+  };
+
+  const formattedReviewDate = (review) => {
+    let reviewDate = review.reviewDate;
+    let stringDate = reviewDate.toString();
+    let formattedDate = stringDate.slice(0, 10).split("-");
+    if (formattedDate.length === 3) {
+      return `${formattedDate[2]}-${formattedDate[1]}-${formattedDate[0]}`;
+    }
+  };
+
   useEffect(() => {
     fetchBook();
     fetchReviews();
@@ -123,7 +143,7 @@ function BookDetailsPage() {
         <h2>{book.book.title}</h2>
         <h3>{book.book.authorId.name}</h3>
         <p>Publisher: {book.book.publisher}</p>
-        <p>Publishing date: {book.book.publishingDate}</p>
+        <p>Publishing date: {formattedPublishingDate()}</p>
         <p>Description: {book.book.description}</p>
 
         {book.book.categories &&
@@ -138,7 +158,7 @@ function BookDetailsPage() {
             <li key={review._id}>
               <img src={review.user?.image} style={{ height: "50px" }} />
               <p>User: {review.user?.userName}</p>
-              <p>Date: {review.reviewDate}</p>
+              <p>Date: {formattedReviewDate(review)}</p>
               <p>Rating: {review.rating}</p>
               <p>Comment: {review.comment}</p>
 
